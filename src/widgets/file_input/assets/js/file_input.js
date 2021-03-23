6a91ead81bcc6f49f39c13a42097762f2ae242f7
Pozitronik <antikillerxxl@gmail.com>
2021-03-23 14:49:39 +0300

    extractFileName function

--- src/widgets/file_input/assets/js/file_input.js
+++ src/widgets/file_input/assets/js/file_input.js
@@ -1,9 +1,19 @@
 /**
  * @param inputElement
  * @param outputElement
- * unused
  */
 function generateOnchangeAction(inputElement, outputElement) {
 	let fileName = extractFileName(inputElement.val());//extractFileName определена в homer.js
 	outputElement.html(fileName + '<br/><span class=\'small text-info\'>файл будет загружен</span>');
+}
+
+/**
+ * @param {string} filePath
+ * @returns {string}
+ */
+function extractFileName(filePath) {
+	let startIndex = (filePath.indexOf('\\') >= 0?filePath.lastIndexOf('\\'):filePath.lastIndexOf('/')),
+		filename = filePath.substring(startIndex);
+	if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) filename = filename.substring(1);
+	return filename;
 }
