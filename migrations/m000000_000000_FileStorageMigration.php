0dc249460bfd218585607ec8c34b9883e43b9bca
Pozitronik <antikillerxxl@gmail.com>
2021-05-05 20:02:47 +0300

    Namespace in migration

--- migrations/m000000_000000_FileStorageMigration.php
+++ migrations/m000000_000000_FileStorageMigration.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types = 1);
 
+namespace pozitronik\filestorage\migrations;
+
 use pozitronik\filestorage\models\FileStorage;
 use pozitronik\filestorage\models\FileTags;
 use yii\db\Migration;
