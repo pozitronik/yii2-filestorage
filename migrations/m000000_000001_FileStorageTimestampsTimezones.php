80972494ff489be76b024f3c4d7df56be860ea7a
Pozitronik <antikillerxxl@gmail.com>
2022-09-15 14:02:39 +0300

    CURRENT_TIMESTAMP by default

--- migrations/m000000_000001_FileStorageTimestampsTimezones.php
+++ migrations/m000000_000001_FileStorageTimestampsTimezones.php
@@ -24,7 +24,7 @@ class m000000_000001_FileStorageTimestampsTimezones extends Migration {
 	 */
 	public function safeUp() {
 		if (null !== ArrayHelper::getValue($this->db->schema->typeMap, 'timestamptz')) {
-			$this->alterColumn(self::mainTableName(), 'at', $this->timestamptz(0)->notNull()->comment('Дата и время создания.'));
+			$this->alterColumn(self::mainTableName(), 'at', $this->timestamptz(0)->defaultExpression('CURRENT_TIMESTAMP')->notNull()->comment('Дата и время создания.'));
 		} else {
 			Yii::info('timestamptz column type is not supported bu DB schema, migration not applied.');
 		}
